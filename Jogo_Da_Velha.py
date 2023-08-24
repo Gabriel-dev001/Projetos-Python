2088081a116ac323b7a0f25f76314a9e5347d5ce
Gabriel-dev001 <gabriel.developer103@gmail.com>
2023-08-23 22:38:56 -0300

    100%

--- Jogo_Da_Velha.py
+++ Jogo_Da_Velha.py
@@ -1,6 +1,5 @@
 import os
 
-
 linha_1 = [" "]*3
 linha_2 = [" "]*3
 linha_3 = [" "]*3
@@ -13,6 +12,15 @@ class Jogo_velha():
     def __init__(self, sinal):
         self.sinal = sinal
 
+    def Tabela(self):
+        Limpa()
+
+        print("    1    2    3")
+        print("1",linha_1)
+        print("2",linha_2)
+        print("3",linha_3)
+        print(f"|{self.sinal}|Escreva sua cordenada. [l,c]")
+
     def Incrementar(self, cordenada):
         lista = cordenada.split(",")
         cord_l = int(lista[0])
@@ -20,72 +28,86 @@ class Jogo_velha():
 
         match(cord_l):
             case 1:
-                linha_1[cord_c-1] = self.sinal
+                if linha_1[cord_c-1] == " ":
+                    linha_1[cord_c-1] = self.sinal
             
             case 2:
-                linha_2[cord_c-1] = self.sinal
+                if linha_2[cord_c-1] == " ":
+                    linha_2[cord_c-1] = self.sinal
 
-            case 3: 
-                linha_3[cord_c-1] = self.sinal
+            case 3:
+                if linha_3[cord_c-1] == " ":
+                    linha_3[cord_c-1] = self.sinal
+        
+        return self.sinal
+
+    def Ganhador(self):
+         print(f"O jogador |{self.sinal}| Ganhou o jogo!!!")
 
              
 bolinha = Jogo_velha("O")
 xzinho = Jogo_velha("X")
+
+
         
             
-def Verificar_ganhador():
-    if linha_1[0] == " " or linha_1[1] == " " or linha_1[2] == " ":
+def Verificar_ganhador(elemento):
+    if linha_1[0] == elemento and linha_1[1] == elemento and linha_1[2] == elemento:
             return False
         
-    if linha_2[0] == " " or linha_2[1] == " " or linha_2[2] == " ":
+    if linha_2[0] == elemento and linha_2[1] == elemento and linha_2[2] == elemento:
             return False
         
-    if linha_3[0] == " " or linha_3[1] == " " or linha_3[2] == " ":
+    if linha_3[0] == elemento and linha_3[1] == elemento and linha_3[2] == elemento:
             return False
         
 
-    if linha_1[0] == " " or linha_2[0] == " " or linha_3[0] == " ":
+    if linha_1[0] == elemento and linha_2[0] == elemento and linha_3[0] == elemento:
         return False
     
-    if linha_1[1] == " " or linha_2[1] == " " or linha_3[1] == " ":
+    if linha_1[1] == elemento and linha_2[1] == elemento and linha_3[1] == elemento:
         return False
     
-    if linha_1[2] == " " or linha_2[2] == " " or linha_3[2] == " ":
+    if linha_1[2] == elemento  and linha_2[2] == elemento and linha_3[2] == elemento:
         return False
     
 
-    if linha_1[0] == " " or linha_2[1] == " " or linha_3[2] == " ":
+    if linha_1[0] == elemento and linha_2[1] == elemento and linha_3[2] == elemento:
             return False
     
-    if linha_1[2] == " " or linha_2[1] == " " or linha_3[0] == " ":
+    if linha_1[2] == elemento and linha_2[1] == elemento and linha_3[0] == elemento:
             return False
     
-    
-    return True
 
 def Jogo():
-    for i in range(9):
-        Limpa()
-        print("    1    2    3")
-        print("1",linha_1)
-        print("2",linha_2)
-        print("3",linha_3)
-        caminho = str(input("Escreva sua cordenada. [l,c]\n"))
+    continuar = True
+    i=0
 
-        if i%2 == 0:
-            xzinho.Incrementar(caminho)
-            ganhador = Verificar_ganhador()
-        
-        else: 
-            bolinha.Incrementar(caminho)
-            ganhador = Verificar_ganhador()
+    while continuar:
+        i+=1
 
-        if ganhador == True:
-            print("O jogo foi ganho!!!")
+        if i == 9:  
+            print("O jogo deu velha!!!")
             break
 
-    print("O jogo deu velha!!!")
+        if i%2 != 0:
+            jogador = xzinho     
+        else: 
+            jogador = bolinha
 
+        jogador.Tabela()        
+        caminho = str(input())
+           
+        sinal = jogador.Incrementar(caminho)
+        ganhador = Verificar_ganhador(sinal)
+
+        if ganhador == False:
+            print(linha_1)
+            print(linha_2)
+            print(linha_3)
+            jogador.Ganhador()
+            break
+ 
 Jogo()
 
 
